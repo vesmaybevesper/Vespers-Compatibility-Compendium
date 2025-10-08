06b6bc4c81bf8b77fc7317b38621a82b1220d770
Ves <vesmaybevesper@gmail.com>
2025-10-08 18:36:11 -0400

    move EMF fix from mixin to event

---
+++ src/client/java/vesper/vcc/leaks/emf/ClearContext.java
@@ -0,0 +1,32 @@
+package vesper.vcc.leaks.emf;
+
+import net.fabricmc.loader.api.FabricLoader;
+import vesper.vcc.events.RenderLivingEvent;
+import vesper.vcc.utils.Util;
+
+import java.lang.reflect.Field;
+
+public class ClearContext {
+
+    public static void init(){
+        if (!FabricLoader.getInstance().isModLoaded("entity_model_features")) return;
+
+        try {
+            Class<?> emfIterationContext = Class.forName("traben.entity_model_features.models.animation.EMFAnimationEntityContext.IterationContext");
+            Field emfHeldIteration = emfIterationContext.getDeclaredField("emf$heldIteration");
+
+            RenderLivingEvent.POST.register((renderer, matrices, vertexConsumers, light, tickDelta) -> {
+                try {
+                   emfHeldIteration.set(renderer, (Object) null);
+                } catch (IllegalAccessException e) {
+                    Util.error("Failed to clear EMF context");
+                }
+            });
+
+        } catch (ClassNotFoundException e) {
+            Util.warn("EMF class not found, skipping EMF fix");
+        } catch (NoSuchFieldException e) {
+            Util.error("Failed to initialize EMF fix");
+        }
+    }
+}
