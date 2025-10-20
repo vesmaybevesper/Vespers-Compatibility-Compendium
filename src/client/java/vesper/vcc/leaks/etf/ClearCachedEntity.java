dcebc58fbf16fd0a7f647d7295dc19c59318c250
Ves <vesmaybevesper@gmail.com>
2025-10-20 14:44:03 -0400

    Update ClearCachedEntity.java

--- src/client/java/vesper/vcc/leaks/etf/ClearCachedEntity.java
+++ src/client/java/vesper/vcc/leaks/etf/ClearCachedEntity.java
@@ -1,12 +1,10 @@
 package vesper.vcc.leaks.etf;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import traben.entity_texture_features.features.player.ETFPlayerTexture;
-import traben.entity_texture_features.utils.ETFEntity;
 import vesper.vcc.events.RenderLivingEvent;
 import vesper.vcc.utils.Util;
 import java.lang.invoke.VarHandle;
@@ -15,24 +13,32 @@ public class ClearCachedEntity {
     private static final Logger LOGGER = LoggerFactory.getLogger("VCC/ETF");
 
     public static void init() {
-        /*if (!FabricLoader.getInstance().isModLoaded("entity_texture_features")) return;
+        if (!FabricLoader.getInstance().isModLoaded("entity_texture_features")) return;
 
-        //Class<?> etfEntity = Class.forName("traben.entity_texture_features.mixin.mixins.entity.renderer.MixinLivingEntityRenderer");
-        VarHandle etfEntity = Util.ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "etf$heldEntity", ETFEntity.class, false);
-        //Field etfHeldEntity = etfEntity.getDeclaredField("etf$heldEntity");
-        //Class<?> etfPlayer = Class.forName("traben.entity_texture_features.mixin.mixins.entity.renderer.MixinPlayerEntityRenderer");
-        VarHandle etfPlayer = Util.ReflectionHelper.getFieldFromClass(PlayerRenderer.class, "etf$ETFPlayerTexture", ETFPlayerTexture.class, false);
-        // Field etfPlayerTexture = etfPlayer.getDeclaredField("etf$ETFPlayerTexture");
+        try {
+            //Class<?> ETF$HELDENTITY = Class.forName("traben.entity_texture_features.mixin.mixins.entity.renderer.MixinLivingEntityRenderer");
+            //VarHandle ETF$HELDENTITY = Util.ReflectionHelper.getFieldFromClass(LivingEntityRenderer.class, "etf$heldEntity", ETFEntityRenderState.class, false);
+            //Field etfHeldEntityField = ETF$HELDENTITY.getDeclaredField("etf$heldEntity");
+            //Class<?> ETF$PLAYERTEXTURE = Class.forName("traben.entity_texture_features.mixin.mixins.entity.renderer.MixinPlayerEntityRenderer");
+            VarHandle ETF$PLAYERTEXTURE = Util.ReflectionHelper.getFieldFromClass(PlayerRenderer.class, "etf$ETFPlayerTexture", ETFPlayerTexture.class, false);
+            //Field etfPlayerTexture = ETF$PLAYERTEXTURE.getDeclaredField("etf$ETFPlayerTexture");
 
-        RenderLivingEvent.POST.register((renderer, matrices, vertexConsumers, light, tickDelta) -> {
-            try {
-                etfEntity.set(renderer, null);
-                if (renderer instanceof PlayerRenderer) {
-                    etfPlayer.set(renderer, null);
+
+            RenderLivingEvent.POST.register((renderer, matrices, vertexConsumers, light, tickDelta) -> {
+                try {
+                    //ETF$HELDENTITY.set(renderer, null);
+                    if (renderer instanceof PlayerRenderer) {
+                        ETF$PLAYERTEXTURE.set(renderer, null);
+                    }
+                } catch (IllegalArgumentException e) {
+                    Util.warn("Failed to clear ETF cache");
                 }
-            } catch (IllegalArgumentException e) {
-                Util.warn("Failed to clear ETF cache");
-            }
-        });*/
+            });
+        } catch (SecurityException e) {
+            Util.error("Error Finding ETF Fields, skipping fixes");
+        }
+
+
+
     }
 }
