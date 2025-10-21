6bbe6457bfa6a0a0d2fe634063133d42697682e1
Ves <vesmaybevesper@gmail.com>
2025-10-21 16:34:44 -0400

    fix crash on Server

--- src/main/java/vesper/vcc/VCC.java
+++ src/main/java/vesper/vcc/VCC.java
@@ -1,16 +1,11 @@
 package vesper.vcc;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.RandomSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vesper.vcc.leaks.supplementaries.ClearRemaining;
 
-import java.util.Arrays;
-
-@Environment(EnvType.CLIENT)
 public class VCC implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("VCC");
     public static final String[] launchMsg = {"I just hit things with hammers until they work", "Oh look! A penny!", "Would you be moved in with by me?", "God has cursed me for my hubris, and my work is never finished"};
