7802dd0da5548320a6ef52709c018ef3a73f2141
Ves <vesmaybevesper@gmail.com>
2025-10-20 21:30:29 -0400

    wow another random launch msg

--- src/main/java/vesper/vcc/VCC.java
+++ src/main/java/vesper/vcc/VCC.java
@@ -8,17 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-//import vesper.vcc.tweaks.enchancement.DashKeyBind;
 
 @Environment(EnvType.CLIENT)
 public class VCC implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("VCC");
-    public static final String[] launchMsg = {"I just hit things with hammers until they work", "Oh look! A penny!", "Would you be moved in with by me?"};
+    public static final String[] launchMsg = {"I just hit things with hammers until they work", "Oh look! A penny!", "Would you be moved in with by me?", "God has cursed me for my hubris, and my work is never finished"};
 
     @Override
     public void onInitialize() {
         int randomMsg = RandomSource.create().nextIntBetweenInclusive(0, launchMsg.length - 1);
-        LOGGER.info(launchMsg[randomMsg]);
-        //DashKeyBind.init();
+        LOGGER.info("[VCC]: {}", launchMsg[randomMsg]);
     }
 }
