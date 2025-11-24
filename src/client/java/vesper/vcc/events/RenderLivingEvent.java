cd9228bd2ff2d15b4218f65d249210de25552450
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-11-24 13:16:08 -0500

    update postRender event for 1.21.10

--- src/client/java/vesper/vcc/events/RenderLivingEvent.java
+++ src/client/java/vesper/vcc/events/RenderLivingEvent.java
@@ -8,11 +8,11 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 
 public interface RenderLivingEvent {
-    Event<RenderLivingEvent> POST = EventFactory.createArrayBacked(RenderLivingEvent.class, (listeners) -> (renderer, matrices, vertexConsumers, light, tickDelta) ->{
+    Event<RenderLivingEvent> POST = EventFactory.createArrayBacked(RenderLivingEvent.class, (listeners) -> (renderer, matrices) ->{
         for (RenderLivingEvent listener : listeners){
-            listener.postRender(renderer, matrices, vertexConsumers, light, tickDelta);
+            listener.postRender(renderer, matrices);
         }
     });
 
-    void postRender(LivingEntityRenderer<?, ?, ?> renderer, PoseStack matrices, MultiBufferSource vertexConsumers, int light, float tickDelta) throws IllegalAccessException;
+    void postRender(LivingEntityRenderer<?, ?, ?> renderer, PoseStack matrices) throws IllegalAccessException;
 }
--- src/client/java/vesper/vcc/mixin/client/events/LivingEntityRendererMixin.java
+++ src/client/java/vesper/vcc/mixin/client/events/LivingEntityRendererMixin.java
@@ -3,10 +3,13 @@ package vesper.vcc.mixin.client.events;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,14 +19,14 @@ import vesper.vcc.events.RenderLivingEvent;
 import vesper.vcc.utils.Util;
 
 @Mixin(LivingEntityRenderer.class)
-public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> implements RenderLayerParent<T, M> {
+public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> extends EntityRenderer<T, S> implements RenderLayerParent<S, M> {
 
     protected LivingEntityRendererMixin(EntityRendererProvider.Context context) {
         super(context);
     }
 
-    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("TAIL"))
-    private void postRender(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) throws IllegalAccessException {
-        RenderLivingEvent.POST.invoker().postRender((LivingEntityRenderer<?, ?>) (Object) this, poseStack, buffer, packedLight, partialTicks);
+    @Inject(method = "submit(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V", at = @At("TAIL"))
+    private void postRender(S livingEntityRenderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState cameraRenderState, CallbackInfo ci) throws IllegalAccessException {
+        RenderLivingEvent.POST.invoker().postRender((LivingEntityRenderer<?, ?, ?>) (Object) this, poseStack);
     }
 }
