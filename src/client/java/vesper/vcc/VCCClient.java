7398237d2b33555ea768a151a3e4bb7e9821cf76
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-11-24 12:07:51 -0500

    remove unused from init

--- src/client/java/vesper/vcc/VCCClient.java
+++ src/client/java/vesper/vcc/VCCClient.java
@@ -2,11 +2,8 @@ package vesper.vcc;
 
 import net.fabricmc.api.ClientModInitializer;
 import vesper.vcc.leaks.emf.ClearContext;
-import vesper.vcc.leaks.emi.EMI;
 import vesper.vcc.leaks.etf.ClearCachedEntity;
 import vesper.vcc.leaks.etf.UpdateEntity;
-import vesper.vcc.leaks.iceberg.ClearEntities;
-import vesper.vcc.leaks.iceberg.ClearMaps;
 import vesper.vcc.leaks.jade.ClearAccessor;
 import vesper.vcc.leaks.jei.ClearCaches;
 import vesper.vcc.leaks.jei.ClearMenu;
@@ -15,12 +12,12 @@ public class VCCClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
         YACLConfig.CONFIG.load();
-        EMI.initialize();
+        //EMI.initialize();
         UpdateEntity.init();
         ClearContext.init();
         ClearCachedEntity.init();
-        ClearMaps.init();
-        ClearEntities.init();
+        //ClearMaps.init();
+        //ClearEntities.init();
         ClearCaches.onLevelUnload();
         ClearCaches.onRespawn();
         ClearMenu.onRespawn();
