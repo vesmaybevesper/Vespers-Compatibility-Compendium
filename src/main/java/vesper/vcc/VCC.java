fd5f422ced8893c1bfc071341d8a13d708a1b3a1
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-11-25 13:03:23 -0500

    better format launchMsg array entries

--- src/main/java/vesper/vcc/VCC.java
+++ src/main/java/vesper/vcc/VCC.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 
 public class VCC implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("VCC");
-    public static final String[] launchMsg = {"I just hit things with hammers until they work", "Oh look! A penny!", "Would you be moved in with by me?", "God has cursed me for my hubris, and my work is never finished", "Don't ask me to do this again"};
+    public static final String[] launchMsg =
+            {"I just hit things with hammers until they work", "Oh look! A penny!", "Would you be moved in with by me?",
+                    "God has cursed me for my hubris, and my work is never finished", "Don't ask me to do this again",
+                    "Toss a coin to your Vesper"};
 
     @Override
     public void onInitialize() {
