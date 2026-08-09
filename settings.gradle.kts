c5b466da52363ccb61043500e33a4392175b8896
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-09 15:35:09 -0400

    set up versions
    I'm not sure if im going to support the forges, I will look at that after i get fabric re-set up and bug free

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,10 +23,10 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
-		match("26.1.2", "fabric", "neoforge")
-		match("1.21.7", "fabric", "neoforge")
-		match("1.21.1", "fabric", "neoforge")
-		match("1.19.2", "fabric", "forge")
+		match("26.2", "fabric")
+		match("1.21.11", "fabric")
+		match("1.21.1", "fabric")
+		match("1.20.1", "fabric")
 
 		vcsVersion = "1.21.1-fabric"
 	}
