26a692f427a30f4147c6d22261b4866e009863ab
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-12 12:38:02 -0400

    clean up droplet mixin

--- src/main/java/dev/vesper/vcc/mixin/effectivexparticlerain/DropletParticleMixin.java
+++ src/main/java/dev/vesper/vcc/mixin/effectivexparticlerain/DropletParticleMixin.java
@@ -1,16 +1,17 @@
 package dev.vesper.vcc.mixin.effectivexparticlerain;
 
-
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import dev.vesper.eveningstarlib.EveningStarLib;
 import dev.vesper.vcc.Config;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.level.block.Blocks;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 //? <26.2{
@@ -40,39 +41,9 @@ public abstract class DropletParticleMixin /*? <=1.21.1{ *//*extends TextureShee
 		super(clientLevel, d, e, f);
 	}
 
-	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-	private void vcc$tick$head(CallbackInfo ci){
-		if (Config.replaceRipple && EveningStarLib.isModLoaded("particlerain") && EveningStarLib.isModLoaded("effective")) {
-			this.xo = this.x;
-			this.yo = this.y;
-			this.zo = this.z;
-			if (this.age++ >= this.lifetime) {
-				this.remove();
-			}
-
-			if (this.onGround || this.age > 5 && this.level.getBlockState(BlockPos.containing(this.x, this.y + this.yd, this.z)).getBlock() == Blocks.WATER) {
-				this.remove();
-			}
-
-			if (this.level.getBlockState(BlockPos.containing(this.x, this.y + this.yd, this.z)).getBlock() == Blocks.WATER && this.level.getBlockState(BlockPos.containing(this.x, this.y, this.z)).isAir()) {
-				for(int i = 0; i > -10; --i) {
-					BlockPos pos = BlockPos.containing(this.x, (double)(Math.round(this.y) + (long)i), this.z);
-					if (this.level.getBlockState(pos).getBlock() == Blocks.WATER && this.level.getBlockState(BlockPos.containing(this.x, (double)(Math.round(this.y) + (long)i), this.z)).getFluidState().isSource() && this.level.getBlockState(BlockPos.containing(this.x, (double)(Math.round(this.y) + (long)i + 1L), this.z)).isAir()) {
-						this.level.addParticle(ParticleRain.RIPPLE, this.x, (double)((float)(Math.round(this.y) + (long)i) + 0.9F), this.z, (double)0.0F, (double)0.0F, (double)0.0F);
-						break;
-					}
-				}
-
-				this.remove();
-			}
-
-			this.xd *= (double)0.99F;
-			this.yd -= (double)0.05F;
-			this.zd *= (double)0.99F;
-			this.move(this.xd, this.yd, this.zd);
-
-			ci.cancel();
-		}
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
+	private void tick(ClientLevel instance, ParticleOptions particle, double x, double y, double z, double xd, double yd, double zd){
+		instance.addParticle(ParticleRain.RIPPLE, x, y, z, xd, yd, zd);
 	}
 	*///?}
 }
