82abfd1d67abf3ebbbeea5300841264b829b8324
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-16 15:19:21 -0400

    call mem leak fixes

--- src/main/java/dev/vesper/vcc/platform/fabric/FabricClientEntrypoint.java
+++ src/main/java/dev/vesper/vcc/platform/fabric/FabricClientEntrypoint.java
@@ -4,6 +4,8 @@ package dev.vesper.vcc.platform.fabric;
 
 import dev.vesper.vcc.VCC;
 import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
+import dev.vesper.vcc.fixes.leaks.emi.ClearEMIHistoryOnRespawn;
+import dev.vesper.vcc.fixes.leaks.etf.UpdateETFEntityOnRespawn;
 import net.fabricmc.api.ClientModInitializer;
 
 @Entrypoint("client")
@@ -12,6 +14,27 @@ public class FabricClientEntrypoint implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		VCC.onInitializeClient();
+
+		// Calls to all the fixes, versioned by highest applicable version, additional versioning may happen within the methods
+		//? <= 1.21.1{
+		/*ClearEMIHistoryOnRespawn.init();
+		*///?} <=1.21.11{
+
+		//?}
+
+		// Calls to fixes that are only applicable to one version
+		//? 1.20.1 {
+
+		//?} 1.21.1 {
+
+		//?} 1.21.11 {
+
+		//?} 26.2{
+
+		//?}
+
+		// All remaining fixes that apply to all versions
+		UpdateETFEntityOnRespawn.init();
 	}
 
 }
