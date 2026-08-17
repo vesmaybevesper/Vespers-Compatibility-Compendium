92c28788f072528e0bcc85f79601c6a0ffad1c99
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-17 14:28:00 -0400

    improve mouth steam mixin

--- src/main/java/dev/vesper/vcc/mixin/effectualxeffective/MouthSteamEffectMixin.java
+++ src/main/java/dev/vesper/vcc/mixin/effectualxeffective/MouthSteamEffectMixin.java
@@ -1,17 +1,17 @@
 package dev.vesper.vcc.mixin.effectualxeffective;
 
 import com.imeetake.effectual.effects.MouthSteam.MouthSteamEffect;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.architectury.registry.registries.RegistrySupplier;
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import dev.vesper.eveningstarlib.EveningStarLib;
 import dev.vesper.vcc.Config;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.particles.SimpleParticleType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 //? 1.20.1{
 /*import org.ladysnake.effective.core.Effective;
@@ -24,18 +24,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MouthSteamEffectMixin {
 
 	//? <=1.21.1{
-	/*@Unique
-	private static final RandomSource RANDOM = RandomSource.create();
-
-	@Inject(method = "spawnBreath", at = @At("HEAD"), cancellable = true)
-	private static void vcc$register$head(Player player, CallbackInfo ci){
+	/*// It's also just straight up not working, might be a velocity issue again
+	@WrapOperation(method = "spawnBreath", at = @At(value = "INVOKE", target = "Lcom/imeetake/effectual/EffectualClientParticles;spawn(Ldev/architectury/registry/registries/RegistrySupplier;DDDDDD)V"))
+	private static void vcc$register$head(RegistrySupplier<? extends SimpleParticleType> type, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original){
 		if (Config.breathSteam && EveningStarLib.isModLoaded("effectual") && EveningStarLib.isModLoaded("effective")) {
-			int count = 3 + RANDOM.nextInt(3);
-
-			for(int i = 0; i < count; ++i) {
-				player.level().addParticle((ParticleOptions) /^? 1.20.1 {^//^Effective.WATERFALL_CLOUD^//^?} 1.21.1 { ^/ /^EffectiveParticles.CASCADE ^//^?} ^/, player.getX(), player.getEyeY(), player.getZ(), (double)player.getId(), (double)0.0F, (double)0.0F);
-			}
-			ci.cancel();
+			assert Minecraft.getInstance().level != null;
+			Minecraft.getInstance().level.addParticle((ParticleOptions) /^? 1.20.1 {^//^Effective.WATERFALL_CLOUD^//^?} 1.21.1 { ^/ /^EffectiveParticles.CASCADE ^//^?} ^/, x, y, z, dx, dy, dz);
+		} else {
+			original.call(type, x, y, z, dx, dy, dz);
 		}
 	}
 	*///?}
