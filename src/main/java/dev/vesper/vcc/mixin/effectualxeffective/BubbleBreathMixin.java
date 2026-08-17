2d012d6cbd56eef9a6755352d60a4504656f22ab
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-17 14:08:20 -0400

    significantly improve bubble breath feature

--- src/main/java/dev/vesper/vcc/mixin/effectualxeffective/BubbleBreathMixin.java
+++ src/main/java/dev/vesper/vcc/mixin/effectualxeffective/BubbleBreathMixin.java
@@ -1,20 +1,16 @@
 package dev.vesper.vcc.mixin.effectualxeffective;
 
 import com.imeetake.effectual.effects.Bubbles.BubbleBreathEffect;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import dev.vesper.eveningstarlib.EveningStarLib;
-import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static dev.vesper.vcc.Config.useEffectiveBubbleBreath;
 
@@ -37,28 +33,12 @@ public class BubbleBreathMixin {
 	/*@Unique
 	private final static RandomSource RANDOM = RandomSource.create();
 
-	@Inject(method = "processBreathTick", at = @At("HEAD"), cancellable = true)
-	private static void vcc$processBreathTick$head(Player player, CallbackInfo ci){
+	@WrapOperation(method = "processBreathTick", at = @At(value = "INVOKE", target = "Lcom/imeetake/effectual/EffectualClientParticles;spawnVanilla(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
+	private static void vcc$processBreathTick$head(ParticleOptions options, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original){
 		if (useEffectiveBubbleBreath && EveningStarLib.isModLoaded("effectual") && EveningStarLib.isModLoaded("effective")){
-			float xRot = player.getXRot();
-			float yRot = player.getYRot();
-			float xRotRad = xRot * ((float)Math.PI / 180F);
-			float yRotRad = -yRot * ((float)Math.PI / 180F);
-			double lookX = (double)(Mth.sin(yRotRad) * Mth.cos(xRotRad));
-			double lookY = (double)(-Mth.sin(xRotRad));
-			double lookZ = (double)(Mth.cos(yRotRad) * Mth.cos(xRotRad));
-			double mouthOffsetForward = (double)0.25F;
-			double mouthOffsetDown = 0.15;
-			double originX = player.getX() + lookX * mouthOffsetForward;
-			double originY = player.getEyeY() - mouthOffsetDown + lookY * mouthOffsetForward;
-			double originZ = player.getZ() + lookZ * mouthOffsetForward;
-			Vec3 playerVel = player.getDeltaMovement();
-			if (RANDOM.nextBoolean()) {
-				double velocityX = lookX * 0.1 + playerVel.x * 0.8;
-				double velocityY = lookY * 0.1 + playerVel.y * 0.8 + 0.05;
-				double velocityZ = lookZ * 0.1 + playerVel.z * 0.8;
 				//? 1.20.1{
-				/^player.level().addParticle((ParticleOptions) Effective.BUBBLE, originX, originY, originZ, velocityX, velocityY, velocityZ);
+			/^assert Minecraft.getInstance().level != null;
+			Minecraft.getInstance().level.addParticle((ParticleOptions) Effective.BUBBLE, x, y, z, dx, dy, dz);
 
 				/^¹WorldParticleBuilder.create(Effective.BUBBLE).enableForcedSpawn()
 						.setScaleData(GenericParticleData.create(0.05f + player.level().random.nextFloat() * 0.05f)
@@ -71,11 +51,11 @@ public class BubbleBreathMixin {
 						.setRenderType(ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT)
 						.spawn(player.level(), originX, originY, originZ);¹^/
 				^///?} 1.21.1{
-				/^player.level().addParticle(EffectiveParticles.BUBBLE, originX, originY, originZ, velocityX, velocityY, velocityZ);
+			/^assert Minecraft.getInstance().level != null;
+			Minecraft.getInstance().level.addParticle(EffectiveParticles.BUBBLE, x, y, z, dx, dy, dz);
 				^///?}
-			}
-
-			ci.cancel();
+		} else {
+			original.call(options, x, y, z, dx, dy, dz);
 		}
 	}
 	*///?}
