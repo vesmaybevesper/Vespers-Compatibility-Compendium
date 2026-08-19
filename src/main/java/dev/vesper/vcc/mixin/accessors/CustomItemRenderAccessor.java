a860098bec4ef9cb6f3c1fc9853b9bade71e3438
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-19 15:00:50 -0400

    fix incorrect versioning of acessor

--- src/main/java/dev/vesper/vcc/mixin/accessors/CustomItemRenderAccessor.java
+++ src/main/java/dev/vesper/vcc/mixin/accessors/CustomItemRenderAccessor.java
@@ -3,18 +3,28 @@ package dev.vesper.vcc.mixin.accessors;
 import com.anthonyhilyard.iceberg.renderer.CustomItemRenderer;
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.Wolf;
-import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+//? <=1.21.1{
+/*import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.animal.horse.Horse;
+*///?} >=1.21.11{
+import net.minecraft.world.entity.animal.equine.Horse;
+import net.minecraft.world.entity.animal.wolf.Wolf;
+//?}
+
 @Mixin(CustomItemRenderer.class)
 @MixinEnvironment(type = MixinEnvironment.Env.CLIENT)
 public interface CustomItemRenderAccessor {
 	@Mutable
-	@Accessor("entity")
+	//? <=1.21.1{
+	/*@Accessor("entity")
+	*///?} >=1.21.11{
+	@Accessor("cachedSpawnEntity")
+	//?}
 	static void setEntity(Entity entity){}
 
 	@Mutable
