f52c6e7a400c862b8eb7e7f8f3ac1387fc40a19e
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-24 19:15:01 -0400

    modify glow drop when effect is redirected

---
+++ src/main/java/dev/vesper/vcc/mixin/particlemod/MixinEffectiveGlowDroplet.java
@@ -0,0 +1,35 @@
+package dev.vesper.vcc.mixin.particlemod;
+
+import dev.vesper.vcc.util.MixinDummy;
+import dev.vesper.vcc.util.ParticleModContext;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.SingleQuadParticle;
+import net.minecraft.client.particle.SpriteSet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+//? 1.20.1{
+/*import org.ladysnake.effective.core.particle.GlowDropletParticle;
+*///?} 1.21.1{
+/*import org.ladysnake.effective.particle.GlowDropletParticle;
+*///?}
+
+//? <=1.21.1 {
+/*@Mixin(GlowDropletParticle.class)
+*///?} >1.21.1{
+@Mixin(MixinDummy.class)
+//?}
+public class MixinEffectiveGlowDroplet {
+
+	//? <=1.21.1 {
+	/*@Inject(method = "<init>", at = @At("TAIL"))
+	private void conditionallyChangeAttributes(ClientLevel world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteSet spriteProvider, CallbackInfo ci){
+		if (ParticleModContext.fixGlowDropForPlayerDrip) {
+			((SingleQuadParticle) (Object) this).quadSize = 0.016f + world.random.nextFloat() * 0.012f;
+			((SingleQuadParticle) (Object) this).alpha = 0.45F + world.random.nextFloat() * 0.2F;
+		}
+	}
+	*///?}
+}
---
+++ src/main/java/dev/vesper/vcc/util/ParticleModContext.java
@@ -0,0 +1,5 @@
+package dev.vesper.vcc.util;
+
+public class ParticleModContext {
+	public static boolean fixGlowDropForPlayerDrip = false;
+}
