eb28681341e3b27fe0bc523dc30e2469b40b4ef9
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-25 00:05:20 -0400

    Update MouthSteamEffectMixin.java

--- src/main/java/dev/vesper/vcc/mixin/effectualxeffective/MouthSteamEffectMixin.java
+++ src/main/java/dev/vesper/vcc/mixin/effectualxeffective/MouthSteamEffectMixin.java
@@ -7,6 +7,7 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import dev.vesper.eveningstarlib.EveningStarLib;
 import dev.vesper.vcc.Config;
+import dev.vesper.vcc.util.ParticleModContext;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.SimpleParticleType;
@@ -28,8 +29,10 @@ public class MouthSteamEffectMixin {
 	private static void vcc$register$invoke(RegistrySupplier<? extends SimpleParticleType> type, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original){
 		if (Config.breathSteam && EveningStarLib.isModLoaded("effectual") && EveningStarLib.isModLoaded("effective")) {
 			assert Minecraft.getInstance().level != null;
-			// the pos needs help to go infront of where the player is looking, particle is too large and moves down and really quickly
+			// the pos needs help to go in front of where the player is looking, particle is too large and moves down and really quickly
+			ParticleModContext.fixCascadeForBreath = true;
 			Minecraft.getInstance().level.addParticle((ParticleOptions) /^? 1.20.1 {^//^Effective.WATERFALL_CLOUD^//^?} 1.21.1 { ^/ /^EffectiveParticles.CASCADE ^//^?} ^/, x, y, z, 0, 0.001, 0);
+			ParticleModContext.fixCascadeForBreath = true;
 		} else {
 			original.call(type, x, y, z, dx, dy, dz);
 		}
