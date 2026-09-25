f4da366b8207da4e00897f2e74b85ee9b7c0ca0b
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-09-25 11:32:51 -0400

    update mouth steam for effectual 1.4.2

--- src/main/java/dev/vesper/vcc/mixin/integrations/effectualxeffective/MouthSteamEffectMixin.java
+++ src/main/java/dev/vesper/vcc/mixin/integrations/effectualxeffective/MouthSteamEffectMixin.java
@@ -6,18 +6,18 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 //?}
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import dev.architectury.registry.registries.RegistrySupplier;
 import dev.kikugie.fletching_table.annotation.MixinEnvironment;
 import dev.vesper.eveningstarlib.EveningStarLib;
-import dev.vesper.vcc.Config;import dev.vesper.vcc.util.ParticleModContext;
+import dev.vesper.vcc.Config;
+import dev.vesper.vcc.util.ParticleModContext;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.SimpleParticleType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 //? 1.20.1 && fabric{
 /*import org.ladysnake.effective.core.Effective;
+import org.spongepowered.asm.mixin.injection.Coerce;
 *///?} 1.21.1 && fabric{
 /*import org.ladysnake.effective.index.EffectiveParticles;
 *///?}
@@ -28,8 +28,8 @@ import org.spongepowered.asm.mixin.injection.At;
 public class MouthSteamEffectMixin {
 
 	//? <=1.21.1 && fabric{
-	/*@WrapOperation(method = "spawnBreath", at = @At(value = "INVOKE", target = "Lcom/imeetake/effectual/EffectualClientParticles;spawn(Ldev/architectury/registry/registries/RegistrySupplier;DDDDDD)V"))
-	private static void vcc$register$invoke(RegistrySupplier<? extends SimpleParticleType> type, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original){
+	/*@WrapOperation(method = "spawnBreath", at = @At(value = "INVOKE", target = "Lcom/imeetake/effectual/effects/MouthSteam/MouthSteamParticleFactory;spawn(DDDDDDLcom/imeetake/effectual/effects/MouthSteam/BreathAnchor;)V"))
+	private static void vcc$register$invoke(double x, double y, double z, double dx, double dy, double dz, @Coerce Object anchor, Operation<Void> original){
 		if (Config.breathSteam() && EveningStarLib.isModLoaded("effectual") && EveningStarLib.isModLoaded("effective")) {
 			assert Minecraft.getInstance().level != null;
 			// the pos needs help to go in front of where the player is looking, particle moves down really quickly
@@ -37,7 +37,7 @@ public class MouthSteamEffectMixin {
 			Minecraft.getInstance().level.addParticle((ParticleOptions) /^? 1.20.1 {^//^Effective.WATERFALL_CLOUD^//^?} 1.21.1 { ^/ /^EffectiveParticles.CASCADE ^//^?} ^/, x, y, z, 0, 0.001, 0);
 			ParticleModContext.fixCascadeForBreath = true;
 		} else {
-			original.call(type, x, y, z, dx, dy, dz);
+			original.call(x, y, z, dx, dy, dz, anchor);
 		}
 	}
 	*///?}
